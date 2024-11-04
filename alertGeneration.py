c3709032d3680e9b61fd237766fab75ab80ad32d
SkyB0i <bigwomble05@gmail.com>
2024-11-04 16:51:05 -0500

    Alert Generation File Logs

--- alertGeneration.py
+++ alertGeneration.py
@@ -1,7 +1,11 @@
 from system_performance import cpu_percentage
 from system_performance import ram_memory
 import log_analysis
+import datetime
 
+alertLog = open('AlertLogs.txt', 'w')
+
+print('Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()))
 def alertGenerator(path_to_log):
     log = log_analysis.readlogs(path_to_log)
     failedlogins, unauthaccess = log_analysis.susactivity(log)
@@ -12,16 +16,16 @@ def alertGenerator(path_to_log):
     unauthorized_access = logSummary["tunauthaccess"]
 
     if failed_logins >= 5:
-        #Code for alert creation
+        alertLog.write('{:%Y-%m-%d %H:%M:%S} [SECURITY ALERT] Excessive failed login attempts detected.'.format(datetime.datetime.now()))
 
     if unauthorized_access >= 2:
-        # Code for alert creation
+         alertLog.write('{:%Y-%m-%d %H:%M:%S} [SECURITY ALERT] Excessive unauthorized access attempts detected.'.format(datetime.datetime.now()))
 
     if cpu_percentage >= 90:
-        # Code for alert creation
+         alertLog.write('{:%Y-%m-%d %H:%M:%S} [RESOURCE ALERT] Excessive CPU use detected.'.format(datetime.datetime.now()))
     
     if ram_memory >= 90:
-        # Code for alert creation
+         alertLog.write('{:%Y-%m-%d %H:%M:%S} [RESOURCE ALERT] Excessive RAM use detected.'.format(datetime.datetime.now()))
 
 path_to_log = "logpath"
 alertGenerator(path_to_log)
