b5d4951ac5711fe0d81797e7beb52d6874f6fbba
noahkaboa <nphinger@gmail.com>
2024-11-04 20:35:28 -0500

    Automated vuln scans

--- automated_security/vuln_scan.py
+++ automated_security/vuln_scan.py
@@ -1,23 +1,39 @@
 import nmap
-import socket 
-hostname = socket.gethostname()
-IPAddr = socket.gethostbyname(hostname)
-scanner = nmap.PortScanner()
-host = 'IPAddr'
-print("Scanning the Following Address:" + IPAddr)
-# Define target IP address or hostname
+import socket
+
+import schedule
+import time
+import logging
 
 
-# Run a basic scan on the target
-scanner.scan(IPAddr)
+ 
 
-# Print the scan results
-for host in scanner.all_hosts():
-    print("Host: ", host)
-    print("State: ", scanner[host].state())
-    for proto in scanner[host].all_protocols():
-        print("Protocol: ", proto)
-        ports = scanner[host][proto].keys()
-        for port in ports:
-            print("Port: ", port, "State: ", scanner[host][proto][port]['state'])
+def vuln_scan():
+    scanner = nmap.PortScanner()
+    hostname = socket.gethostname()
+    IPAddr = socket.gethostbyname(hostname)
     
+    # Run a basic scan on the target
+    host = 'IPAddr'
+    logging.info("Scanning the Following Address:" + IPAddr)
+    scanner.scan(IPAddr)
+
+    # Print the scan results
+    for host in scanner.all_hosts():
+        logging.info("Host: ", host)
+        logging.info("State: ", scanner[host].state())
+        for proto in scanner[host].all_protocols():
+            logging.info("Protocol: ", proto)
+            ports = scanner[host][proto].keys()
+            for port in ports:
+                logging.info("Port: ", port, "State: ", scanner[host][proto][port]['state'])
+    
+def main():
+    logname = "vuln_scan.log"
+    logging.basicConfig(
+        filename=logname,
+        filemode="a",
+        level=logging.INFO,
+        format="%(asctime)s - {levelname} - %(message)s",
+    )
+    schedule.every().hour.do(vuln_scan)
